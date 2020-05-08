d7c221da5b8d3418f52e53d8b8f0e35899aa5f57
Stephane Selim <stephan.selim@epfl.ch>
2020-05-08 20:22:54 +0200

    save work on lark grammar tryouts

--- examples/lark_grammar_tryouts.py
+++ examples/lark_grammar_tryouts.py
@@ -1,29 +1,160 @@
-import sys
-from lark import Lark, Transformer
+#
+# This example demonstrates usage of the Indenter class.
+#
+# Since indentation is context-sensitive, a postlex stage is introduced to
+# manufacture INDENT/DEDENT tokens.
+#
+# It is crucial for the indenter that the NL_type matches
+# the spaces (and tabs) after the newline.
+#
 
-# A simple grammar to test indentation and optional braces
+from pathlib import Path
 
-json_grammar = r"""
-    ?value: dict
-          | string
-          | SIGNED_NUMBER      -> number
-          | "true"             -> true
-          | "false"            -> false
-          | "null"             -> null
+from lark import Lark, Transformer, v_args
+from lark.indenter import Indenter
 
-    dict : "{" [pair ("," pair)*] "}" | [pair ("," pair)*]
-    pair : string ":" value
+python_grammar_file = Path('examples/python.lark')
 
-    string : ESCAPED_STRING
+tree_grammar_mapping = r"""
+    ?start: _NL* _value
+    _value : mapping* | NAME 
+    mapping: NAME ":" _mapping_helper _NL*
+    _mapping_helper: NAME | _NL _INDENT mapping+ _DEDENT 
+    %import common.CNAME -> NAME
+    %import common.WS_INLINE
+    %declare _INDENT _DEDENT
+    %ignore WS_INLINE
+    _NL: /(\r?\n[\t ]*)+/
+"""
 
-    %import common.ESCAPED_STRING
-    %import common.SIGNED_NUMBER
-    %import common.WS
-    %ignore WS
-    """
+tree_grammar_seq = r"""
+    ?start: _NL* _value
+    _value : seq* | name 
+    seq : seq_item+
+    seq_item: "-" _seq_helper _NL*
+    _seq_helper : name | _NL _INDENT seq_item+ _DEDENT 
+    name : NAME
+    %import common.CNAME -> NAME
+    %import common.WS_INLINE
+    %declare _INDENT _DEDENT
+    %ignore WS_INLINE
+    _NL: /(\r?\n[\t ]*)+/
+"""
 
-json_parser = Lark(json_grammar, start='value', lexer='standard')
+tree_grammar_2 = r"""
+    ?start: _NL* mapping
+    mapping: NAME ":" _value
+    _value: _NL [_INDENT mapping+ _DEDENT] | NAME
+    %import common.CNAME -> NAME
+    %import common.WS_INLINE
+    %declare _INDENT _DEDENT
+    %ignore WS_INLINE
+    _NL: /(\r?\n[\t ]*)+/
+"""
 
-test_data = '"key": "item0"'
+tree_grammar = r"""
+    ?start: _NL* tree
+    tree: NAME _NL [_INDENT tree+ _DEDENT]
+    %import common.CNAME -> NAME
+    %import common.WS_INLINE
+    %declare _INDENT _DEDENT
+    %ignore WS_INLINE
+    _NL: /(\r?\n[\t ]*)+/
+"""
 
-tree = json_parser.parse(test_data)
+class TreeIndenter(Indenter):
+    NL_type = '_NL'
+    OPEN_PAREN_types = []
+    CLOSE_PAREN_types = []
+    INDENT_type = '_INDENT'
+    DEDENT_type = '_DEDENT'
+    tab_len = 8
+
+class PythonIndenter(Indenter):
+    NL_type = '_NEWLINE'
+    OPEN_PAREN_types = ['LPAR', 'LSQB', 'LBRACE']
+    CLOSE_PAREN_types = ['RPAR', 'RSQB', 'RBRACE']
+    INDENT_type = '_INDENT'
+    DEDENT_type = '_DEDENT'
+    tab_len = 8
+
+python_parser = Lark.open(
+    python_grammar_file,
+    parser='lalr', 
+    postlex=PythonIndenter(),
+    start='file_input'
+)
+
+parser = Lark(tree_grammar_seq, parser='lalr', postlex=TreeIndenter(), debug=True)
+
+class TreeToSomething(Transformer):
+
+    @v_args(inline=True)
+    def name(self, string):
+        print("visiting name: ", string)
+        (s,) = string
+        return s
+
+    def seq(self, items):
+        print("visiting seq: ", items)
+        return items
+
+    def seq_item(self, items):
+        print("visiting seq items: ", items)
+        return items
+
+    null = lambda self, _: None
+    true = lambda self, _: True
+    false = lambda self, _: False
+
+test_python = """
+if 2 == 2:
+    True
+else :
+    False
+"""
+
+test_tree_mapping = """
+a : 
+    b : c
+    d : e
+f : g
+"""
+
+test_tree_seq = """
+- 
+  - a
+  - b
+- d
+"""
+
+test_tree_2 = """
+a:
+    b: bae
+    c:
+        d:boy
+        e:girl
+    f:
+        g:wat
+"""
+
+test_tree = """
+a
+    b
+    c
+        d
+        e
+    f
+        g
+"""
+
+def test():
+    parse_tree = parser.parse(test_tree_seq)
+    print(parse_tree.pretty())
+    print(TreeToSomething().transform(parse_tree))
+
+#def test():
+#    print(python_parser.parse(test_python).pretty())
+
+if __name__ == '__main__':
+    test()
